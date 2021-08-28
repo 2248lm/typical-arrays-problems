983bb310d157dccb37d7bbf5358a506bc3c5ec2b
Lazzat Mukhametalinova <2248lm@gmail.com>
2021-08-28 17:18:58 +0600

    feat: add solution of the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length < 1) return 0;
+  return (array.sort((a, b) => a - b)[0]);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length < 1) return 0;
+  return (array.sort((a, b) => b - a)[0]);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length < 1) return 0;
+  let sum = 0;
+  array.map(e => sum = sum + e);
+  return (sum / array.length);
 }
